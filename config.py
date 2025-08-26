af9b523ec5fbf20d6ded74f6c7b59a76711d7a05
Kartik10S <sehgalk116@gmail.com>
2025-08-26 11:15:57 +0530

    with api key

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 # First, try to get values from environment (Heroku)
 telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 telegram_chatid = os.getenv("TELEGRAM_CHATID")
+api_football_key = os.getenv("API_FOOTBALL_KEY") # New key for standings
 
 # Fallback for local testing (only used if env vars are not set)
 if not telegram_bot_token:
@@ -10,3 +11,6 @@ if not telegram_bot_token:
 
 if not telegram_chatid:
     telegram_chatid = "YOUR_CHAT_ID_HERE"
+
+if not api_football_key:
+    api_football_key = "ee281508f95bc44b1901a25fd669baba"
